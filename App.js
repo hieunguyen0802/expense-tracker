24e6d17d962db030e890247baea0b2b98e158f96
hieunguyen0802 <hieu.h.t.nguyen@tcu.edu>
2024-05-07 18:03:01 +0700

    install dependencies

--- App.js
+++ App.js
@@ -1,12 +1,34 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { PaperProvider } from 'react-native-paper';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { Button, Checkbox } from 'react-native-paper';
+import { useState } from 'react';
 
 export default function App() {
+
+  const [checked, setChecked] = useState(false)
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <PaperProvider>
+      <SafeAreaProvider>
+        <View style={styles.container}>
+          <Text>Open up App.js to start on your app!</Text>
+            <Checkbox
+                status={checked ? 'checked' : 'unchecked'}
+                onPress={() => {
+                  setChecked(!checked);
+                }}
+                color='red'
+              />
+            <Button icon="camera" mode="contained" onPress={() => console.log('Pressed')}>
+              Press me
+            </Button>
+           
+          <StatusBar style="auto" />
+        </View>
+        </SafeAreaProvider>
+      </PaperProvider>
   );
 }
 
